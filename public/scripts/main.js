1b2b100d442bb19a7f05b071d75b2f5be9080fd7
kou72 <genpokopokopoko@gmail.com>
2020-11-08 19:10:41 +0900

    fast commit

---
+++ public/scripts/main.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// Initializes
+function UrlShortner() {
+  // Shortcuts to DOM Elements.
+  this.input = document.getElementById("input");
+  this.output = document.getElementById("output");
+  this.snackbar = document.getElementById("snackbar");
+  this.shortenButton = document.getElementById("shorten");
+
+  this.shortenButton.addEventListener("click", this.shortenUrl.bind(this));
+  const buttonTogglingHandler = this.toggleButton.bind(this);
+  this.input.addEventListener("change", buttonTogglingHandler);
+  this.input.addEventListener("keyup", buttonTogglingHandler);
+}
+
+// Shorten the URL
+UrlShortner.prototype.shortenUrl = async function (e) {
+  e.preventDefault();
+  const apiurl = "apigateway URL"
+  const param = {
+    method: "POST",
+    mode: "cors",
+    body: this.input.value
+  }
+  try {
+    const response = await fetch(apiurl, param)
+    if (!response.ok) throw "ok propertyi is false"
+    const body = await response.json()
+    this.output.value = body
+    this.output.focus()
+    this.successSnackbar();
+
+  } catch (err) {
+    console.log(err)
+    this.errorSnackbar();
+  }
+};
+
+UrlShortner.prototype.toggleButton = function () {
+  if (this.input.value) {
+    this.shortenButton.removeAttribute("disabled");
+  } else {
+    this.shortenButton.setAttribute("disabled", "true");
+  }
+};
+
+UrlShortner.prototype.successSnackbar = function () {
+  this.snackbar.style.backgroundColor = "#2196f3";
+  this.snackbar.MaterialSnackbar.showSnackbar({message: "URLを短縮しました"});
+};
+
+UrlShortner.prototype.errorSnackbar = function () {
+  this.snackbar.style.backgroundColor = "#f44336";
+  this.snackbar.MaterialSnackbar.showSnackbar({message: "処理に失敗しました"});
+};
+
+window.onload = function () {
+  // Initializes MailForm.
+  window.UrlShortner = new UrlShortner();
+};
